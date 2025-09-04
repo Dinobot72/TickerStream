491c9bd30259c72613b49cb4dd6fa8b0080ce767
Dylan Regan <dyregan6706@gmail.com>
2025-09-04 12:46:35 -0600

    modern dashboard setup

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,14 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import { importProvidersFrom } from '@angular/core';
+import { MatGridListModule } from '@angular/material/grid-list';
 
-bootstrapApplication(App, appConfig)
+bootstrapApplication(App, {
+  providers: [
+    importProvidersFrom(
+      MatGridListModule,
+    )
+  ],
+})
   .catch((err) => console.error(err));
