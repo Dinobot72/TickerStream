a1adf659c87a16519946f2d79080139ae13c5e5f
Dylan Regan <dyregan6706@gmail.com>
2025-09-04 22:18:34 -0600

    move main-content to subfolder

--- frontend/src/app/dashboard/dashboard.component.ts
+++ frontend/src/app/dashboard/dashboard.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, computed, signal } from '@angular/core'
 import { MatGridListModule } from '@angular/material/grid-list';
 import { SidebarComponent } from './sidebar/sidebar.comonent';
+import { MainContentComponent } from './main-content/main-conent.component';
 
 
 @Component({
@@ -11,19 +12,12 @@ import { SidebarComponent } from './sidebar/sidebar.comonent';
         CommonModule,
         MatGridListModule,
         SidebarComponent,
+        MainContentComponent
     ],
     templateUrl: './dashboard.component.html',
     styleUrls: ['./dashboard.component.scss'],
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DashboardComponent {
-    portfolioValue = signal(5234.20)
-    portfolioPercentage = computed(() => {
-        const target = 10000;
-        return Math.floor((this.portfolioValue() / target) * 100);
-    });
-    portfolioProgress = computed(() => {
-        const circumference = 251.2;
-        return circumference - (circumference * this.portfolioPercentage()) / 100;
-    });
+    
 }
