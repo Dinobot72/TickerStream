74b6a34d274a951c17028f7f0a9a06c2912eddef
Dylan Regan <dyregan6706@gmail.com>
2025-09-22 12:38:01 -0600

    adding routing

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,13 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { HttpClient } from '@angular/common/http'
 
 @Component({
   selector: 'app-root',
   imports: [
     RouterOutlet,
-    DashboardComponent,
   ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
