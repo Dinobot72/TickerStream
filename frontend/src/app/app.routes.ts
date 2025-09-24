d7319615680614f155c4a2f029f0426be62a3b31
Dylan Regan <dyregan6706@gmail.com>
2025-09-24 12:42:54 -0600

    trying to remove ssr

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,8 +4,10 @@ import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
 export const routes: Routes = [
+    {path: '', redirectTo: '/login', pathMatch: 'full'},
     {path: 'login', component: LoginComponent},
     {path: 'dashboard', component: DashboardComponent},
+    {path: '**', redirectTo: 'login'}
 ];
 
 @NgModule({
