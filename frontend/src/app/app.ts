1bf2416c696accd0bb143af07398820c6516b149
Dylan Regan <dyregan6706@gmail.com>
2025-09-25 11:33:54 -0600

    Fixed the double rendering of the application

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -3,11 +3,9 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [
-    RouterOutlet,
-  ],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrls: ['./app.scss']
 })
 export class App {
   protected readonly title = signal('stockBot');
