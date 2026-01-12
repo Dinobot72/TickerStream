a8ae59468286dfdb96638e931ade09648407261e
Dylan Regan <dyregan6706@gmail.com>
2026-01-12 10:31:17 -0700

    Authentication Guard revamp and test coverage

--- frontend/src/app/auth-guard.ts
+++ frontend/src/app/auth-guard.ts
@@ -8,18 +8,5 @@ export const authGuard: CanActivateFn = (route, state) => {
   const authService = inject(AuthService);
   const router = inject(Router);
 
-  return authService.isAuthenticated().pipe(
-    map ( isAuthenticated => {
-      if (isAuthenticated) {
-        return true;
-      } else {
-        router.navigate(['/login']);
-        return false;
-      }
-    }),
-    catchError(() => {
-      router.navigate(['/login']);
-      return of(false);
-    })
-  )
+  return authService.isAuthenticated() ? true : router.parseUrl('/login');
 };
