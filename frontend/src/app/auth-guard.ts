7f9f22fed9d838d0e5d7ebb63b6cad164a96755a
Dylan Regan <dyregan6706@gmail.com>
2026-01-14 13:04:32 -0700

    package updates

--- frontend/src/app/auth-guard.ts
+++ frontend/src/app/auth-guard.ts
@@ -2,7 +2,6 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from './auth.service';
-import { catchError, map, of } from 'rxjs';
 
 export const authGuard: CanActivateFn = (route, state) => {
   const authService = inject(AuthService);
